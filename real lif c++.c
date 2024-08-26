47fe8857327de746d3ccd7ee3214a159e0eef26d
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 11:29:51 +0530

    Create real lif c++.c

---
+++ real lif c++.c
@@ -0,0 +1,11 @@
+// if the person is old enough to vote
+
+#include <iostream>
+using namespace std;
+int main (){
+int myAge = 19;
+int votingAge = 18;
+cout <<((myAge)>=votingAge) ;
+return 0;
+
+}
